c3a86db729227687c2be3c74149ed6fd8606d852
LucasSouza2021 <jadson.l.souza@ba.estudante.senai.com.br>
2023-05-16 22:33:47 -0300

    Adicionando comentários

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-window.addEventListener('load', async () => {
-    let i = 1
-    let data;
+window.addEventListener('load', async () => { //Faz a requisição dos dados assim que a tela é carregada
+    let i = 1 //Será o id das rows
+    let data; //Armazenará os dados vindos da API
     const pDate = document.querySelector('#date')
 
     await fetch('https://covid19-brazil-api.now.sh/api/report/v1')
     .then(res => res.json())
     .then(json => data = json.data)
 
-    pDate.innerHTML = pDate.innerHTML + FormatDate(data[0].datetime)
+    pDate.innerHTML = pDate.innerHTML + FormatDate(data[0].datetime) //Mostra na tela a data da ultima atualização da API
 
-    data.forEach((item) => {
+    data.forEach((item) => { //Percorre cada item do objeto, passando por parâmetro o id da tabela, as informações contidas no objeto e seu id
         addRow('table', item, i)
         i++
     })
 })
 
-const FormatDate = (date) => {
-    function FixDigits(num){
+const FormatDate = (date) => { // Formata a data em dd/mm/yyyy
+    function FixDigits(num){ //Adiciona um 0 caso o dd ou mm tenha somente um dígito
         return num.toString().length === 1 ? '0' + num : num
     }
 
     let dateInstance = new Date(date)
     dateInstance = `${FixDigits(dateInstance.getDate())}/${FixDigits(dateInstance.getMonth() + 1)}/${dateInstance.getFullYear()}`
-    return dateInstance
+    return dateInstance //Retorna em dd/mm/yyyy
 }
 
-function addRow(tableID, data, id) {
+function addRow(tableID, data, id) { //Adiciona as rows na tabela
     const table = document.querySelector('#' + tableID)
     const newRow = table.insertRow(-1)
 
-    const cells = {
+    const cells = { //Objeto que contém as células das rows
         id: newRow.insertCell(0),
         uf: newRow.insertCell(1),
         state: newRow.insertCell(2),
@@ -48,7 +48,7 @@ function addRow(tableID, data, id) {
     addDataOnRows(id, data.refuses, cells.refuses)
 }
 
-function addDataOnRows(id, data, cell){
+function addDataOnRows(id, data, cell){ //Adiciona os dados nas rows, e define sua cor de fundo com base no seu id
     id % 2 === 1 ? cell.style.backgroundColor = '#f2f2f2' : null
 
     let text = document.createTextNode(data)
